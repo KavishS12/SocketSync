65db3876998d32f57256db8f593209f2ed1784a7
KavishS12 <shahkavish0809@gmail.com>
2025-06-16 22:45:31 +0530

    minor change

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -88,7 +88,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.cookie("jwt", "", {maxAge: 0});
+    res.cookie("jwt", "",  {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 0
+    });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.error("Error during logout:", error.message);
